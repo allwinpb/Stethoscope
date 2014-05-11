3ada014c5803e36075f1f6859e6544b73a8eb6a4
Allwin <allwinpb@gmail.com>
2014-05-12 00:42:21 +0800

    RAM and CPU usage added for win32

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 var app = require("express")();
+var os = require("./stats.js")();
 
 app.route('/').get(function(req,res,err){
-	res.send("Hello world");
+	res.send(JSON.stringify(os));
 });
 
 app.listen(8000)
---
+++ engines/win32.js
@@ -0,0 +1,43 @@
+var monitor = {};
+var safeToUpdate = 0;
+
+var cmd = require("child_process").exec
+
+function sanitize(input){
+	return input.split('\n')[1].split(' ')[0]
+}
+
+function update(){
+	if(safeToUpdate <= 0){
+		console.log('Updating');
+		safeToUpdate = 2;
+		cmd("wmic cpu get loadpercentage", function(error, stdout, stderr){
+			monitor.cpuPercentage = parseInt(sanitize(stdout));
+			safeToUpdate -= 1;
+		});
+		cmd("wmic os get freephysicalmemory", function(error, stdout, stderr){
+			monitor.ramFree = parseInt(sanitize(stdout));
+			monitor.ramPercentage = monitor.ramFree / monitor.ramMax;
+			safeToUpdate -= 1;
+		});
+	}
+}
+
+function setup(){
+	cmd("wmic os get totalvisiblememorysize", function(error, stdout, stderr){
+		monitor.ramMax = parseInt(sanitize(stdout));
+	});
+}
+
+function init(){
+	monitor.cpuPercentage = 0;
+	monitor.ramFree = 0;
+	monitor.ramMax = 1;
+	monitor.ramPercentage = 0;
+	setup();
+	setInterval(update,1000);
+	update();
+}
+
+init();
+module.exports = monitor;
