286fce862d602dd5221efe38c9e8690a50be9b93
Allwin <allwinpb@gmail.com>
2014-05-11 22:37:24 +0800

    Basic skeleton + README added

---
+++ README.md
@@ -0,0 +1,3 @@
+Node server to export specific computer statistics in JSON format
+
+Originally envisioned to allow display of important computer information on a second screen (such as a tablet or a smartphone) which has network access to the host computer.
---
+++ stats.js
@@ -0,0 +1,12 @@
+var os = require('os')
+module.exports = function(){
+	var engine;
+	if(os.platform() == 'win32'){
+		engine = require('./engines/win32.js')
+	}else if(os.platform() == 'linux'){
+		engine = require('./engines/linux.js')
+	}else if(os.platform() == 'osx'){
+		engine = require('./engines/osx.js')
+	}
+	return engine;
+}
