32571248b7a1c67bd25ad31d6d194957b40ed582
Allwin <allwinpb@gmail.com>
2014-05-18 23:34:59 +0800

    Demo client skeleton added

--- app.js
+++ app.js
@@ -4,5 +4,7 @@ var os = require("./stats.js")();
 app.route('/').get(function(req,res,err){
 	res.send(JSON.stringify(os));
 });
-
+app.route('/client').get(function(req,res,err){
+	res.sendfile('./client.html');
+})
 app.listen(8000)
