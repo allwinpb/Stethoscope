51211300db2b583f1508cc00c4a242c28b86e67c
Allwin Panikulam <allwinpb@gmail.com>
2014-05-27 18:41:10 +0800

    The server is now automatically started on server start and returns a function to get the data

--- stats.js
+++ stats.js
@@ -1,12 +1,19 @@
-var os = require('os')
+var engine;
+var platform = require('os').platform();
+
+if(platform == 'win32'){
+	console.log("OS: Windows")
+	engine = require('./engines/win32.js')
+}else if(platform == 'linux'){
+	console.log("OS: Linux")
+	engine = require('./engines/linux.js')
+}else if(platform == 'darwin'){
+	console.log("OS: Mac OS X")
+	engine = require('./engines/osx.js')
+}
+
+console.log("Starting Stethoscope...")
+
 module.exports = function(){
-	var engine;
-	if(os.platform() == 'win32'){
-		engine = require('./engines/win32.js')
-	}else if(os.platform() == 'linux'){
-		engine = require('./engines/linux.js')
-	}else if(os.platform() == 'osx'){
-		engine = require('./engines/osx.js')
-	}
 	return engine();
 }
